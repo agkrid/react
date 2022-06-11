acd3920ec9c74d70db27c109617d497d154575b0
agkrid <63926970+agkrid@users.noreply.github.com>
2022-06-11 23:31:54 +0200

    f

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+
+rootElement.render(<App />);
